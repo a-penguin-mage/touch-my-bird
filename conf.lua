53a537019b23336a1952657708d000ad8cabb309
penginmage <cpnava@gmail.com>
2018-02-13 22:12:11 +0800

    fixed love version msg; fixed apk build

--- conf.lua
+++ conf.lua
@@ -2,7 +2,7 @@
 function love.conf(t)
   SF = 0.6;  
 	t.title = "touch my bird" -- The title of the window the game is in (string)
-	t.version = "0.10.2"         -- The LÖVE version this game was made for (string)
+	t.version = "0.10.1"         -- The LÖVE version this game was made for (string)
 	t.window.width = 720*SF
 	t.window.height = 1080*SF
 
